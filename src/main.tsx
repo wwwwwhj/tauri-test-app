1ec960875c1e0e72480316cee393d3d85a8b7c9f
wwwwwhj <1648184868@qq.com>
2026-09-11 16:57:33 +0800

    style: load branches view styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./Detail.css";
 import "./WorkingTree.css";
+import "./Branches.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
