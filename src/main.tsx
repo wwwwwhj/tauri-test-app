633dfb0bcdc18608bf19d80b9f68b50f57132f59
wwwwwhj <1648184868@qq.com>
2026-09-11 18:14:49 +0800

    feat: wire application settings provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { AppSettingsProvider } from "./appSettings";
 import { I18nProvider } from "./i18n";
 import { ThemeProvider } from "./theme";
 import "./Detail.css";
@@ -12,12 +13,16 @@ import "./WorkbenchPolish.css";
 import "./UiPreferences.css";
 import "./ThemeMode.css";
 import "./ThemeLightFix.css";
+import "./SettingsDialog.css";
+import "./AppSettings.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider>
       <I18nProvider>
-        <App />
+        <AppSettingsProvider>
+          <App />
+        </AppSettingsProvider>
       </I18nProvider>
     </ThemeProvider>
   </React.StrictMode>,
