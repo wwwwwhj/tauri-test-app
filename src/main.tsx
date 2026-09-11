f577e0d9a7b5d06a2bbcf11289f806b7417d51f5
wwwwwhj <1648184868@qq.com>
2026-09-11 18:05:49 +0800

    feat: wire i18n and theme providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { I18nProvider } from "./i18n";
+import { ThemeProvider } from "./theme";
 import "./Detail.css";
 import "./WorkingTree.css";
 import "./Branches.css";
 import "./GitInfoCache.css";
 import "./WorkbenchTheme.css";
 import "./WorkbenchPolish.css";
+import "./UiPreferences.css";
+import "./ThemeMode.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <I18nProvider>
+        <App />
+      </I18nProvider>
+    </ThemeProvider>
   </React.StrictMode>,
 );
