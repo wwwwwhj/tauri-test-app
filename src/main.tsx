1a765525843234d3ad9b4aeeb07d261f5ff8dcce
wwwwwhj <1648184868@qq.com>
2026-09-11 18:00:50 +0800

    style: load unified workbench theme last

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./Detail.css";
 import "./WorkingTree.css";
 import "./Branches.css";
 import "./GitInfoCache.css";
+import "./WorkbenchTheme.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
